03435c56aa1dbf4467420631cc5c7096b9a1ea39
Clement Owireku-Bogyah <89473856+Zena4L@users.noreply.github.com>
2023-03-09 22:33:46 +0000

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for a in range(122, 96, -1):
-    if a % 2 is 1:
-        a -= 32
+for a in reversed(range(ord("a"), ord("z") + 1)):
+    if a % 2 != 0:
+        a = a - 32
     print("{:c}".format(a), end="")
